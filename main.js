307e6c170c7eefa9c53e8ff4d4bea96e9ac8692c
muvuquinha <drielly.figueiredoo@gmail.com>
2021-11-04 21:48:40 -0300

    codição aula 04.11

---
+++ main.js
@@ -0,0 +1,38 @@
+const a = '10';
+if (a ==10) { 
+       console.log("os valores são iguais");
+} else if (a > 10){   
+  console.log("a é maior que 10");
+}else {   
+  console.log("a variavel a não é igual 10 e não é maior qe 10");
+}
+
+//switch
+ const nota = 6;
+ switch(nota){
+     case 4:
+     console.log("reprovado!");
+     break; 
+     case 6:
+     console.log("recuperação");
+     break;
+     default: 
+     console.log("nota acima da media");
+     break;
+
+     
+ }
+     
+     const b = '10';
+if (b == 10) { 
+       console.log("os valores são iguais");
+} else{
+  console.log(" os valores são diferentes");
+ }
+
+ let c = b == 16 ? "É igual a 10" : "São diferentes";
+ console.log(c);
+ 
+ let resultado = 3 == 4 ? "3 é diferente de 4" : "3 é igual a 4";
+
+ 
